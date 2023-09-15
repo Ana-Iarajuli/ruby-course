7053c1bf98ba65e4a67c8c0654a42e0f0c710738
Ana-Iarajuli <anna.iarajuli@gmail.com>
2023-09-15 10:13:02 +0400

    finished debugging

---
+++ learn-to-code-with-ruby/11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,9 @@
+require "debug"
+
+candy = "some kind of candy"
+
+puts "Don't take candy #{candy} from strangers"
+
+debugger
+
+puts "this is the end"
---
+++ learn-to-code-with-ruby/11 - Debugging/the_step_keyword.rb
@@ -0,0 +1,9 @@
+require "debug"
+
+puts "im not hot enough to compete with io"
+debugger
+3.times do |count|
+    puts "On number #{count}"
+    puts "some"
+    
+end
